85b8f26ebd31cf0dffa9a2d8de21d49cf90c7588
dreamweaver1231_1 <pratapsingh.infy@gmail.com>
2016-01-09 12:00:38 +0000

    initial commit

---
+++ client/app/app.js
@@ -0,0 +1,20 @@
+'use strict';
+
+angular.module('workspaceApp', [
+  'workspaceApp.auth',
+  'workspaceApp.admin',
+  'workspaceApp.constants',
+  'ngCookies',
+  'ngResource',
+  'ngSanitize',
+  'btford.socket-io',
+  'ui.router',
+  'ui.bootstrap',
+  'validation.match'
+])
+  .config(function($urlRouterProvider, $locationProvider) {
+    $urlRouterProvider
+      .otherwise('/');
+
+    $locationProvider.html5Mode(true);
+  });
